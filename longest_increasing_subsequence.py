54a8bcc80cd916c128375a5b7b9739d177b14d0a
mittgaurav <gm_5315@yahoo.com>
2019-02-23 12:04:00 +0530

    longest non-repeating substring

--- longest_increasing_subsequence.py
+++ longest_increasing_subsequence.py
@@ -4,19 +4,12 @@ Created on Sun Nov 11 20:22:40 2018
 
 @author: gaurav
 """
-# NOT CORRECT
 
 
 def longest_incr_subseq(arr):
     """longest subsequence (can be
     non-consecutive) such that all
     elems are in increasing order"""
-
-    # should not go sequentially
-    # backward as we end up with
-    # terms that larger than me.
-    # Instead, jump back to vals
-    # that are smaller than me.
     if len(arr) == 0:
         return 0
 
@@ -25,6 +18,10 @@ def longest_incr_subseq(arr):
 
     val = 0
     for i in range(len(arr)):
+        # for all values before
+        # me, if i am more than
+        # them, i can use their
+        # longest_subseq plus 1
         for j in range(i):
             if arr[i] >= arr[j]:
                 val = max(1+longest_incr_subseq(arr[:j+1]), val)
@@ -35,3 +32,42 @@ def longest_incr_subseq(arr):
 print(longest_incr_subseq([2, 3, 1, 6, 9, 5]))
 print(longest_incr_subseq([2, 3, 1, 6]))
 print(longest_incr_subseq([10, 22, 9, 33, 21, 50, 41, 60, 80]))
+
+
+def longest_non_repeating_str(string):
+    """find the longest non-repeating substring"""
+    if not string:
+        return ""
+    if len(string) == 1:
+        return string
+
+    last_loc = {}  # track last location of char
+    begin = []  # beginning of non-repeating str
+
+    for i in range(len(string)):
+        if string[i] not in last_loc:
+            # not seen this char so update last
+            # loc and take begin from previous
+            begin.append(0 if i is 0 else begin[i-1])
+        else:
+            # update last_loc. begin is
+            # max of next from last_loc
+            # or begin for previous one
+            last = last_loc[string[i]]
+            begin.append(max(last + 1, begin[i-1]))
+        last_loc[string[i]] = i
+
+    maxlen = 0
+    endloc = 0
+    for i in range(len(begin)):
+        if (i - begin[i] + 1) > maxlen:
+            maxlen = i - begin[i] + 1
+            endloc = i
+
+    # now pick maxlen behind start len
+    return string[endloc - maxlen + 1:endloc + 1]
+
+
+print(longest_non_repeating_str("abcded"))
+print(longest_non_repeating_str("abcdedefgnhxzabc"))
+print(longest_non_repeating_str("abcdedefgnhabc"))
---
+++ skyline.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 16 22:56:07 2019
+
+@author: gaurav
+"""
+
+
+def skyline(arr):
+    """A skyline for given
+    building dimensions"""
+    stack = list()
+    result = list()
+
+    now = 0
+    while now < len(arr):
+        start, height, end = arr[now]
+        print(start, height)
+
+        nxt = now + 1
+        while nxt < len(arr):
+            n_start, n_height, n_end = arr[nxt]
+
+            # next building is far away
+            if n_start >= end:
+                break
+
+            # next building is big, so we take it.
+            if n_height > height:
+                now = nxt
+                if end > n_end:  # Add to stack
+                    stack.append((n_end, height, end))
+                now -= 1
+                break
+
+            if n_end > end:
+                stack.append((end, n_height, n_end))
+            nxt += 1
+            now += 1
+        now += 1
+
+
+A = [
+     (1, 10, 4),
+     (3, 15, 8),
+     (6, 9, 11),
+     (10, 12, 15),
+     (18, 12, 22),
+     (20, 8, 25),
+     ]
+skyline(A)
