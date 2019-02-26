7f95151cb8b20451a8c55cf350e111c267b77dcc
mittgaurav <gm_5315@yahoo.com>
2019-02-27 02:37:37 +0530

    Move array to one place

--- array_problems.py
+++ array_problems.py
@@ -49,6 +49,98 @@ def overlapping_area_of_rects(rect1, rect2):
     return (rx - lx) * (ry - ly)
 
 
+print("==", overlapping_area_of_rects.__name__, "==")
 print(overlapping_area_of_rects(((2, 1), (5, 5)), ((3, 2), (5, 7))))
 print(overlapping_area_of_rects(((2, 1), (6, 8)), ((3, 2), (5, 5))))
 print(overlapping_area_of_rects(((2, 1), (4, 8)), ((3, 2), (5, 6))))
+
+
+def duplicate_ints(arr):
+    """does array contain
+    duplicate nums"""
+    seen = {}
+    for v in arr:
+        if v in seen:
+            return True
+        seen[v] = None
+    return False
+
+
+print("====duplicate_ints====")
+print(duplicate_ints([1, 2, 3, 4, 5, 6]))
+print(duplicate_ints([1, 2, 3, 4, 5, 6, 1]))
+
+
+def find_int_with_sum(arr, v):
+    """find two ints with sum v"""
+    seen = {}
+    for a in arr:
+        if v - a in seen:
+            return True
+
+        seen[a] = None
+
+    return False
+
+
+print("====find_int_with_sum====")
+print(find_int_with_sum([1, 2, 3, 4, 5, 6, 1, 4, 6, 9], 12))
+print(find_int_with_sum([1, 2, 3, 4, 5, 6, 1, 4, 6, 9], 1))
+print(find_int_with_sum([1, 2, 3, 4, 5, 6, 1, 4, 6, 9], 2))
+
+
+def print_sorted_in_range(arr, a, b):
+    """get sorted elements
+    between a and b"""
+    if a > b:  # make sure a <= b
+        a, b = b, a
+
+    # quick sort 3 way
+    i = 0
+    L = 0
+    H = len(arr) - 1
+    while i <= H:
+        if arr[i] < a:  # first bucket
+            # we copy the first element in
+            # second bucket to ith and ith
+            # to Lth as L'll be increased
+            arr[i], arr[L] = arr[L], arr[i]
+            i += 1  # next element
+            L += 1  # first bucket increased
+        elif arr[i] > b:  # third bucket
+            arr[i], arr[H] = arr[H], arr[i]
+            H -= 1  # third bucket increased
+        else:  # second bucket
+            i += 1
+
+    new = arr[L:H+1]
+    new.sort()  # sort only limited size
+    return new
+
+
+print("====print_sorted_in_range====")
+print(print_sorted_in_range([1, 2, 4, 5, 3, 6, 1, 4, 6, 9], 3, 5))
+
+
+def longest_increasing_subarray(arr):
+    """longest_increasing subarray"""
+    if len(arr) <= 1:
+        return len(arr)
+
+    last = arr[0]
+    ret = 1
+    max_ret = 1
+    for i in arr[1:]:
+        if i >= last:
+            ret += 1
+        else:
+            max_ret = max(max_ret, ret)
+            ret = 1
+        last = i
+
+    return max(max_ret, ret)
+
+
+print("====longest_increasing_subarray====")
+print(longest_increasing_subarray([1, 2, 3, 4, 5, 6, 1, 4, 6, 9]))
+print(longest_increasing_subarray([1, 2, 3, 4, 5]))
