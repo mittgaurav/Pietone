0e1ebe66bd8ef12249291c40a582d9ca9f9249dd
mittgaurav <gm_5315@yahoo.com>
2019-02-27 22:38:45 +0530

    Another attempt to fix skyline
    doesn't work still

--- skyline.py
+++ skyline.py
@@ -9,11 +9,10 @@ Created on Sat Feb 16 22:56:07 2019
 # NOT CORRECT
 # NOT CORRECT
 
+
 def skyline(arr):
     """A skyline for given
     building dimensions"""
-    stack = list()
-    result = list()
 
     now = 0
     while now < len(arr):
@@ -21,30 +20,37 @@ def skyline(arr):
         print(start, height)
 
         nxt = now + 1
-        while nxt < len(arr):
+        while nxt < len(arr):  # ignore short and small
             n_start, n_height, n_end = arr[nxt]
 
-            # next building is far away
+            # next building is far away, so read
             if n_start >= end:
                 break
 
-            # next building is big, so we take it.
+            # next building is big, so we take it
             if n_height > height:
                 now = nxt
-                if end > n_end:  # Add to stack
-                    stack.append((n_end, height, end))
+                if end > n_end:  # Add my remains
+                    arr.insert(nxt, (n_end, height, end))
                 now -= 1
                 break
 
+            # next building is small but longer
+            # so keep what'll remain after this
             if n_end > end:
-                stack.append((end, n_height, n_end))
+                arr[nxt] = (end, n_height, n_end)
+                break
+
             nxt += 1
             now += 1
+
         now += 1
 
 
 A = [
      (1, 10, 4),
+     (2, 5, 3),
+     (2.5, 5, 8),
      (3, 15, 8),
      (6, 9, 11),
      (10, 12, 15),
