3be1a1a0b0b91966b17294145c617aeb3a5f04ce
mittgaurav <gm_5315@yahoo.com>
2019-03-05 10:08:03 +0530

    Don't need cache

--- power_a_to_b.py
+++ power_a_to_b.py
@@ -7,30 +7,25 @@ Created on Sun Mar  3 13:57:40 2019
 cache half power results. O(log n)
 """
 
-CACHE = None
-
 
 def power(a, b):
     """power without pow() under O(b)"""
     if a == 0:
         return 0
 
-    if b in CACHE:  # power in cache
-        return CACHE[b]
-
     if b == 1:
-        CACHE[b] = a
+        return a
     elif b == 0:
-        CACHE[b] = 1
+        return 1
     elif b < 0:
-        CACHE[b] = 1 / power(a, -b)
+        return 1 / power(a, -b)
     else:  # positive >1 integer
         half_res = power(a, b // 2)
-        CACHE[b] = half_res * half_res
+        ret = half_res * half_res
         if b % 2:  # odd power
-            CACHE[b] *= a
+            ret *= a
 
-    return CACHE[b]
+    return ret
 
 
 for A, B in [(4, 5), (2, 3), (2, -6), (12, 4)]:
