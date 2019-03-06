d585f4a62852ca2cb6df7ba77ff1045e7d8860c0
mittgaurav <gm_5315@yahoo.com>
2019-03-06 08:53:25 +0530

    Add a psycho algo around duplicate nums

--- skyline.py
+++ skyline.py
@@ -47,14 +47,90 @@ def skyline(arr):
         now += 1
 
 
-A = [
-     (1, 10, 4),
+A = [(1, 10, 4),
      (2, 5, 3),
      (2.5, 5, 8),
      (3, 15, 8),
      (6, 9, 11),
      (10, 12, 15),
      (18, 12, 22),
-     (20, 8, 25),
-     ]
+     (20, 8, 25)]
+print(skyline.__name__)
 skyline(A)
+print("----------------")
+
+
+def main(dict_bank_list_of_times):
+    """
+    Bank hours problem
+    Given a list of banks opening hours, determine
+    the hours when there is at least one open bank
+
+    This is useful to determine when it's possible
+    to submit an order into a trading system.
+
+    Example
+    JPDL: 8-12 13-17
+    BARX: 9-13 14-18
+
+    Result: 8-18
+    """
+    times = []
+
+    for bank_times in dict_bank_list_of_times.values():
+        times.extend(bank_times)
+
+    times.sort(key=lambda x: x[0])
+
+    res = []
+
+    start = 9999999
+    end = 0
+    while times:
+        my_times = times.pop(0)
+        start = min(start, my_times[0])
+        end = max(end, my_times[1])
+        if times:  # not the last element
+            if end < times[0][0]:  # there is gap between
+                # current element end and next start. End
+                # right now and collect this into result
+                res.append((start, end))
+                start = 9999999
+                end = 0
+
+    if start != 9999999:
+        # there has been no gap
+        res.append((start, end))
+
+    print(res)
+
+
+print("bank_open_times")
+main({"JPDL": [(8, 12), (13, 17)], "BARX": [(9, 13), (14, 18)]})
+main({"JPDL": [(8, 12), (13, 17)], "BARX": [(9, 13), (19, 20)]})
+main({"JPDL": [(8, 12), (13, 17), (19, 19)], "BARX": [(9, 13)]})
+main({"JPDL": [(8, 12), (13, 17), (19, 19)], "BARX": []})
+main({})
+print("----------------")
+
+
+def duplicate_nums_n(arr):
+    """in an unsorted array from 1 to n-1
+    find duplicates"""
+    res = set()
+    for i in range(len(arr)):
+        if arr[abs(arr[i])] < 0:
+            res.add(i)
+        else:
+            arr[i] = -arr[i]
+
+    return res
+
+
+print(duplicate_nums_n.__name__, duplicate_nums_n([2, 3, 3, 1]))
+print("----------------")
+
+
+def first_missing_num_n(arr):
+    """in an unsorted array from 1 to n-1
+    find the smallest element missing"""
