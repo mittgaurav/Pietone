187fe2bd51401390f967accba6e3e53dfe471567
mittgaurav <gm_5315@yahoo.com>
2019-03-19 11:58:26 +0530

    matrix spiral print

--- matrix_shell_rotation.py
+++ matrix_shell_rotation.py
@@ -71,3 +71,24 @@ assert [print(_) for _ in matrix]
 matrix_rotation(matrix, 100)
 print("-----")
 assert [print(_) for _ in matrix]
+print("-----")
+
+
+def matrix_spiral_print(matrix, rs, re, cs, ce):
+    """print spiral matrix"""
+    if rs > re or cs > ce:
+        return
+    for i in range(rs, re+1):
+        print(matrix[i][cs], end=" ")
+    for i in range(cs+1, ce+1):
+        print(matrix[re][i], end=" ")
+    for i in range(re-1, rs-1, -1):
+        print(matrix[i][ce], end=" ")
+    for i in range(ce-1, cs, -1):
+        print(matrix[rs][i], end=" ")
+
+    matrix_spiral_print(matrix, rs+1, re-1, cs+1, ce-1)
+
+
+print(matrix_spiral_print.__name__)
+matrix_spiral_print(matrix, 0, len(matrix)-1, 0, len(matrix[0])-1)
