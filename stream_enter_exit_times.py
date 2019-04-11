d6c4da2c17841562284e66d86175f7b6c2188cd5
mittgaurav <gm_5315@yahoo.com>
2019-04-12 02:18:40 +0530

    online users at a given time
    given login and logout times

--- stream_enter_exit_times.py
+++ stream_enter_exit_times.py
@@ -4,7 +4,9 @@ Created on Wed Apr 10 23:22:48 2019
 
 @author: gaurav
 """
+import random
 from datetime import datetime
+from binary_searches import binary_search_greater, binary_search_ge
 
 STREAMS = [  # user id - step - time - enter?
     (10, 1, datetime(2014, 4, 11, 8, 0), True),
@@ -53,4 +55,80 @@ def get_avg_time_spent():
     print("cur_step:", curr_step)
 
 
+print("====", get_avg_time_spent.__name__)
 get_avg_time_spent()
+print("=======================")
+
+# Given logs of start and end
+# times of users logins, find
+# how many users were online
+# at a given instant of time
+BEGIN = 0
+END = 100
+TIMES = []
+for _ in range(END // 2):
+    start = random.randint(BEGIN, END)
+    end = random.randint(start, END)
+    TIMES.append((start, end))
+
+
+def online_at_given_time(t):
+    """num of users online
+    at a given time"""
+    starts = sorted(map(lambda x: x[0], TIMES))
+    ends = sorted(map(lambda x: x[1], TIMES))
+
+    # how many have entered till now.
+    # people entering right now are also in
+    enters = binary_search_greater(starts, t)
+    enter = enters[0] if enters[0] != -1 else len(starts)
+
+    # how many have exited till now.
+    # people exiting right now are still in
+    exits = binary_search_ge(ends, t)
+    exitt = exits[0] if exits[0] != -1 else len(ends)
+
+    return enter - exitt
+
+
+print(sorted(TIMES, key=lambda x: x[0]))
+print("==", online_at_given_time.__name__)
+print(0, online_at_given_time(0))
+print(10, online_at_given_time(10))
+print(9, online_at_given_time(9))
+print(49, online_at_given_time(49))
+print(100, online_at_given_time(100))
+print(101, online_at_given_time(101))
+print("=======================")
+
+
+def online_at_any_time():
+    """num of users online
+    at any time. cached"""
+    starts = sorted(map(lambda x: x[0], TIMES))
+    ends = sorted(map(lambda x: x[1], TIMES))
+
+    statuses = [0] * (END + 1)
+    for i in range(BEGIN, END + 1):
+        if i > 0:
+            statuses[i] = statuses[i-1]
+        while starts and starts[0] == i:
+            statuses[i] += 1
+            starts.pop(0)
+
+        while ends and ends[0] == i-1:
+            statuses[i] -= 1
+            ends.pop(0)
+
+    return lambda t: statuses[t]
+
+
+online_at_given_time = online_at_any_time()
+print("==", online_at_any_time.__name__)
+print(0, online_at_given_time(0))
+print(10, online_at_given_time(10))
+print(9, online_at_given_time(9))
+print(49, online_at_given_time(49))
+print(100, online_at_given_time(100))
+# print(101, online_at_given_time(101))
+print("=======================")
