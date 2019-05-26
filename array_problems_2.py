7301f1049e9d68f1d1c789f151b980a9f09843ba
mittgaurav <gm_5315@yahoo.com>
2019-05-26 22:15:53 +0530

    place pair of ints at ith distance
    backtracking problem

--- array_problems_2.py
+++ array_problems_2.py
@@ -27,6 +27,33 @@ def string_in_order(order, string):
     return True
 
 
+print("====", string_in_order.__name__)
 print(string_in_order('abc', 'aaa'))
 print(string_in_order('abc', 'cabc'))
 print(string_in_order('abc', 'xadb'))
+
+
+def place_i_spaced_ints(arr, n):
+    """place pair of n ints spaced
+    at ith distance from each other"""
+    if n == 0:
+        return True
+
+    for i in range(0, len(arr) - n - 1):
+        if arr[i] == 0 and arr[i+n+1] == 0:
+            arr[i], arr[i+n+1] = n, n
+            if not place_i_spaced_ints(arr, n-1):
+                # failure, backtrack
+                arr[i], arr[i+n+1] = 0, 0
+
+    # check that n exists
+    # twice in the array
+    count = len([x for x in arr if x == n])
+    return count == 2
+
+
+print("====", place_i_spaced_ints.__name__)
+array = [0] * 2 * 3
+print(place_i_spaced_ints(array, 3), array)
+array = [0] * 2 * 4
+print(place_i_spaced_ints(array, 4), array)
