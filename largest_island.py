f55c8e40fc19518515fc17a84596e11a8feede76
mittgaurav <gm_5315@yahoo.com>
2020-05-08 02:58:23 +0100

    Update largest_island.py

--- largest_island.py
+++ largest_island.py
@@ -78,3 +78,79 @@ for i in range(len(area)):
         res = max(res, count_nation(i, j, area[i][j]))
 
 print(res)
+
+
+def paths(i, j, n):
+    """given a rectangular/square board,
+    tell how many ways can we traverse.
+    This only allows going down or right"""
+    if i == n - 1 and j == n - 1:
+        return 1
+
+    def f1():
+        if i < n - 1 and j < n - 1:
+            return paths(i + 1, j, n) + paths(i, j + 1, n)
+        if i == n - 1 and j < n - 1:
+            return paths(i, j + 1, n)
+        if i < n - 1 and j == n - 1:
+            return paths(i+1, j, n)
+
+    def f2():
+        """less code but slower
+        because of recursion"""
+        if i > n or j > n:
+            return 0
+        return paths(i + 1, j, n) + paths(i, j + 1, n)
+
+    return f1()
+
+
+print(paths.__name__)
+print(2, paths(0, 0, 2))
+print(3, paths(0, 0, 3))
+print(7, paths(0, 0, 7))
+print(10, paths(0, 0, 10))
+
+scores = None
+
+
+def path_all_direction(i, j, n):
+    """given a rectangular/square board,
+    tell how many ways can we traverse.
+    We can go in any direction"""
+#    if scores[i][j] != -1:  # memoization
+#        return scores[i][j]
+
+    if i == n-1 and j == n-1:
+        return 1
+
+    visited[i][j] = True
+
+    def allowed(ii, jj):
+        if ii < 0 or ii >= n:
+            return False
+        if jj < 0 or jj >= n:
+            return False
+
+        return True
+
+    res = 0
+    if allowed(i+1, j) and not visited[i+1][j]:
+        res += path_all_direction(i+1, j, n)
+    if allowed(i, j+1) and not visited[i][j+1]:
+        res += path_all_direction(i, j+1, n)
+    if allowed(i-1, j) and not visited[i-1][j]:
+        res += path_all_direction(i-1, j, n)
+    if allowed(i, j-1) and not visited[i][j-1]:
+        res += path_all_direction(i, j-1, n)
+
+    visited[i][j] = False
+    scores[i][j] = res
+    return res
+
+
+print(path_all_direction.__name__)
+N = 4
+scores = [[-1 for _ in range(N)] for _ in range(N)]
+visited = [[False for _ in range(N)] for _ in range(N)]
+print(path_all_direction(0, 0, N))
