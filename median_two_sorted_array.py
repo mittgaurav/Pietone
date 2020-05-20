cb67fe2a4385812254398059246c693e3096ac3a
mittgaurav <gm_5315@yahoo.com>
2020-05-20 03:09:47 +0100

    corrected median of two sorted arrays

--- median_two_sorted_array.py
+++ median_two_sorted_array.py
@@ -22,22 +22,30 @@ def median_two_sorted_array(arr, brr):
     j = 0
     k = 0
     median = 0
-    while True:
-        if arr[i] <= brr[j]:
+    for k in range(0, second+1):
+        # second is the larger of two
+        # indices, so we go till that
+        if j == len(brr) or arr[i] <= brr[j]:
             val = arr[i]
             i += 1
-        else:
+        elif i == len(arr) or arr[i] > brr[j]:
             val = brr[j]
             j += 1
 
+        # we need this twice since first
+        # can be equal to second - thus,
+        # we have to capture both so we
+        # don't miss having them twice.
         if k == first:
             median += val / 2
         if k == second:
             median += val / 2
-            break
-        k += 1
 
     return median
 
 
 print(median_two_sorted_array([1, 3, 5], [2, 4, 6]))
+print(median_two_sorted_array([1, 1, 5], [2, 4, 6]))
+print(median_two_sorted_array([1, 3, 5, 12, 33, 122, 56], [2, 4, 6]))
+print(median_two_sorted_array([], []))
+print(median_two_sorted_array([1], []))
