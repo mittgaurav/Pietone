46d982d9c06c489f9cb270998663ec66053b7010
mittgaurav <gm_5315@yahoo.com>
2020-05-31 00:25:35 +0100

    k array merge sort using Heap
    heap of pointers with top element pointing to one with the next larger element

--- merge_sort.py
+++ merge_sort.py
@@ -3,6 +3,7 @@
 merge sort
 @author: gaurav
 """
+from priority_queue import Heap
 
 
 def merge(arr, brr):
@@ -18,6 +19,8 @@ def merge(arr, brr):
             ret.append(brr[j])
             j = j + 1
 
+    # exhaust remaining
+    # elements, if any
     while i < len(arr):
         ret.append(arr[i])
         i = i + 1
@@ -44,3 +47,36 @@ print(mergesort([0, 10, 2, 23, 5, -8, 1, 12]))
 print(mergesort([0]))
 print(mergesort([0, 0, 0, 0, 0]))
 print(mergesort([-8, 0, 1, 2, 5, 10, 12, 23]))
+
+
+def kmergesort(arrays):
+    """ Given k sorted arrays, merge
+    them in a single sorted array"""
+
+    class ThisHeap(Heap):
+        """Array with minimum value at root"""
+        @classmethod
+        def order(cls, arr, parent, child):
+            """parent <= child in min heap"""
+            return arrays[arr[parent]][0] <= arrays[arr[child]][0]
+
+    heap = ThisHeap()
+    result = []
+
+    # consider arrays that are not empty
+    [heap.add(i) for i in range(len(arrays)) if arrays[i]]
+
+    while heap.vals:
+        val = heap.pop()
+
+        result.append(arrays[val].pop(0))
+
+        # add this guy back
+        # at its new location
+        if arrays[val]:
+            heap.add(val)
+
+    return result
+
+
+print(kmergesort([[1, 4, 7], [2, 5, 8], [3, 6, 9]]))
