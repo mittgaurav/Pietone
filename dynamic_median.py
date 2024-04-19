74e8084701837241218157861f043cd18894e1be
mittgaurav <gm_5315@yahoo.com>
2024-04-19 23:00:10 +0100

    Keep track of Kth largest element
    This can be modified to keep a running counter and add/pop elements.

--- dynamic_median.py
+++ dynamic_median.py
@@ -18,7 +18,7 @@ import numpy as np
 
 
 class TrackMedian():
-    """tracks median using
+    """tracks median via
     min and max heaps"""
     def __init__(self):
         self.min = MinHeap()  # upper half
@@ -59,8 +59,45 @@ class TrackMedian():
 
 A = TrackMedian()
 B = list()
+print("======= Track Median =======")
 for i in [6, 1, 3, 5, 7, 4, 8, 2, 9]:
     A.add(i)
     B.append(i)
     B.sort()
-    print(B, "==", A.get_median(), "|", np.median(B))
+    print(B, "=>", A.get_median())
+    assert A.get_median() == np.median(B)
+
+
+class KthLargest():
+    """Keep track of kth largest
+    number, don't care about the
+    rest. If yes, then store num
+    array and use it to add vals
+    when a new number is put."""
+    def __init__(self, array, K):
+        self.min = MinHeap()  # smallest of larger
+        self.K = K
+
+        for i in array:
+            self.add(i)
+
+    def __str__(self):
+        return str(self.min)
+
+    __repr__ = __str__
+
+    def add(self, elem):
+        """add new elem"""
+        self.min.add(elem)
+        if len(self.min.vals) > self.K:
+            # Heap has now more than k
+            self.min.pop()
+
+        return self.min.peek()
+
+
+print("======= Kth Largest ========")
+A = KthLargest([3, 1, 5, 12, 2, 11], 4)
+print(A)
+for i in [6, 6, 10, 1, 3, 7, 8, 20, 30]:
+    print(A, A.add(i))
