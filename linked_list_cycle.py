29a1de0f5bd829df504cf8b28220de7ea8bdf6d5
mittgaurav <gm_5315@yahoo.com>
2024-05-09 08:42:35 +0100

    merge two sorted linked lists.

--- linked_list_cycle.py
+++ linked_list_cycle.py
@@ -6,6 +6,7 @@ Created on Sun May 26 03:49:02 2019
 """
 
 import unittest
+from linked_list import ListNode as L
 
 
 def contains_cycle(first_node):
@@ -19,12 +20,9 @@ def contains_cycle(first_node):
     while True:
         slow = slow.next
         fast = fast.next
-        if fast:
-            fast = fast.next
-        if not fast:
-            return False
-        if fast == slow:
-            return True
+        if fast: fast = fast.next
+        if not fast: return False
+        if fast == slow: return True
     return False
 
 
@@ -98,3 +96,40 @@ class Test(unittest.TestCase):
 
 
 unittest.main(verbosity=2)
+
+
+def merge_sorted_list(i, j):
+    if not i: return j
+    if not j: return i
+
+    # ensure that i is the smaller one
+    result = i = i if i.val <= j.val else j
+
+    prev = None
+    while i and j:
+        if i.val <= j.val:
+            # i is still smaller
+            # hence, keep moving
+            prev = i
+            i = i.next
+        else:
+            # j is smaller - in this case
+            # we have to swap i and j.
+            # Cut connection between prev
+            # and i, and point prev to j.
+            # Now j.next is i and i is j.
+            # It's delicate manipulation.
+            prev.next = j
+            prev = prev.next
+            j = i
+            i = prev.next
+
+    if i: prev.next = i
+    if j: prev.next = j
+    return result
+
+
+i = L(1, L(3, L(7, L(15, L(18)))))
+j = L(2, L(8, L(9, L(10))))
+print(i, j)
+print(merge_sorted_list(i, j))
